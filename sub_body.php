87fa94a8d3f1af977ae78d7b5b5f683d5d00d65b
Jaswanth_Yenduri <jaswanth.y@tcs.com>
2022-04-12 23:02:27 +0530

    first commit

---
+++ sub_body.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FOODCUBO</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+  <link href="dist/jquery-backToTop.min.css" rel="stylesheet" type="text/css">
+
+  <link rel="stylesheet" type="text/css" href="">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+ 	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0/flatly/bootstrap.min.css" rel="stylesheet">
+ 	<link href="dist/css/component-slidebars.min.css" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="styles.css">
+  <meta property="og:description" name="description" content="Floating Social Share Buttons">
+  <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/d/d6/Hs-2009-25-e-full_jpg.jpg">
+  
+  
+<style type="text/css">
+  
+  body{
+
+  background-image: url("b5.jpg");
+  background-attachment: fixed;
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 95%;
+  margin: auto;
+}
+#aaaa{
+  display: none;
+}
+#lo_go{
+ background-image: url('logo2.png');
+ background-attachment: fixed;
+  
+  background-repeat: no-repeat;
+background-position: center;
+height: 100%;
+  }
+
+</style>
+
+</head>
+<body style="">
+
+<div class="sub_body">
+<!--dig_in-->
+<div class="dig">
+<div class="dig_sub">
+<center>
+  <div class="dig_title" style="font-size: 60px;text-shadow: 2px 2px 2px black;">
+      <p id="js_to_title">HUNGRY! THEN LETS DIG IN THROUGH ONLINE</p>
+  </div>
+</center>
+<div class="container-fluid dig_images">
+          <div class="row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-2 col-md-2 col-lg-2 dig_in_images">
+                      <a href="#food_section" style="color: white;text-shadow: 1px 1px 2px white;">
+                          <center>
+                                <img src="dig_food.jpg" class="image" style="width:100%;height:150px;border-radius: 10px;">
+                              </center>
+                              <center>
+                                  <div class="dig_data" style="color: black;"><b style="">FOOD</b></div>
+                                <div class="middle" style="width: 85%;">
+                                           <div class="tt" style="padding-top: 28px;"><b>FOOD</b></div>
+                                          <div style="text-shadow: 1px 1px 2px white;">We provide all kinds of foods from your favorite restaurant</div>
+                                </div>
+                              </center>
+
+
+
+                      </a>
+                </div>
+<pre>   </pre>
+          <div class="col-sm-2 col-md-2 col-lg-2 dig_in_images">
+                       <a href="#city_section" style="color: white;text-shadow: 1px 1px 2px white;">
+                        <center><img src="dig_city.jpg"  class="image" style="width:100%;height:150px;border-radius: 10px"></center>
+                        <center>
+                                <div class="dig_data" style="color: black;"><b>CITIES</b></div>
+                                     <div class="middle" style="width: 85%;">
+                                        <div style="padding-top: 30px;text-shadow: 1px 1px 2px white;"> CITIES</div>
+                                        <div style="">We provide services in 40+ cities</div>
+                                      </div>
+                        </center>
+                       </a>
+         </div>
+  <pre>   </pre>
+
+                    <div class="col-sm-2 col-md-2 col-lg-2 dig_in_images">
+                         <a href="#service_section" style="color: white;text-shadow: 1px 1px 2px white;">
+                                <center><img src="dig_service.jpg" class="image" style="width:100%;height:150px;border-radius: 10px"></center>
+                                <center>
+                                       <div class="dig_data" style="color: black;"><b>SERVICES</b></div>
+                                      <div class="middle" style="width: 85%;">
+                                            <div style="padding-top: 30px;"> SERVICES</div>
+                                            <div style="">24/7 service at your door</div>
+                                      </div>
+                                </center>
+                          </a>
+                    </div>
+<pre>   </pre>
+                  <div class="col-sm-2 col-md-2 col-lg-2 dig_in_images">
+                        <a href="#contact_section" style="color: white;text-shadow: 1px 1px 2px white;">  
+                            <center><img src="dig_contacts.jpg" class="image" style="width:100%;height:150px;border-radius: 10px"></center>
+                             <center>
+                                <div class="dig_data" style="color: black;"><b>CONTACTS</b></div>
+                                <div class="middle" style="width: 85%;">
+                                  <div style="padding-top: 30px;text-shadow: 1px 1px 2px white;"> CONTACTS</div>
+                                  <div style="">feel free to contact us for any time</div>
+                                </div>
+                              </center>
+                        </a>
+                  </div>
+  <pre>   </pre>
+<div class="col-sm-2"></div>
+</div>
+</div>
+</div>
+</div>
+<!--dig_in-->
+
+<!--for the bottom to the 0p-->
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+<script src="jquery-backToTop.js"></script>
+<script>
+var $button = $.backToTop({theme: 'fawesome'});
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!---->
+
+
+</body>
+
+</html>
